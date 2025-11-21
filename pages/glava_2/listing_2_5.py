9e27adede8eaffc0d81d15fc36143f8205e6f8ba
APostolit <50382487+APostolit@users.noreply.github.com>
2025-11-21 11:22:15 +0000

    Add files via upload

---
+++ pages/glava_2/listing_2_5.py
@@ -0,0 +1,23 @@
+import base64
+import streamlit as st
+
+# Задать имя PDF файла
+pdf_file = 'pdf/Python_vsem.pdf'
+
+# Открыть PDF файл
+with open(pdf_file, "rb") as f:
+    base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+
+# Создать объект для вывода в HTML
+# pdf_d = (f'<iframe src="data:application/pdf;base64,{base64_pdf}"'
+         # f' width="1000" height="800" type="application/pdf"></iframe>')
+# pdf_d = (f'<embed src="data:application/pdf;base64,{base64_pdf}"'
+         # f' width="1000" height="800" type="application/pdf">')
+# st.pdf(pdf_file, height=800)
+pdf_d = (f'<embed src="data:application/pdf;base64,{base64_pdf}" width="1000" height="800"'
+         f' alt="pdf" pluginspage="http://www.adobe.com/products/acrobat/readstep2.html">')
+
+st.subheader(":blue[Содержимое PDF файла с оглавлением]")
+
+# Отобразить содержимое PDF файла
+st.markdown(body=pdf_d, unsafe_allow_html=True)
